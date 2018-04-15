fbf31488f6f608949d1adeb574f6698fc1d29923
Pallavi <pbenawri@umich.edu>
2018-04-15 19:13:56 -0400

    Trials new

--- Trials/trial.js
+++ Trials/trial.js
@@ -1,9 +1,16 @@
-$(document).ready(function(){
-	console.log("done");
-	$('p').click(function(){
-		$(this).hide();
-	});
+// $(document).ready(function(){
+// 	console.log("done");
+// 	$('p').click(function(){
+// 		$(this).hide();
+// 	});
 
-});
+// });
 
 document.getElementById('hello').innerHTML="Hey you";
+
+function hellochange(){
+
+	var x = document.getElementById('hello');
+	x.style.color= 'blue';
+
+}
