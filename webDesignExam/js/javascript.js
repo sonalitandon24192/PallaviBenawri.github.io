3dfb8ffce805dea56a63b857e0a2374334b73905
Pallavi <pbenawri@umich.edu>
2018-04-16 11:26:26 -0400

    Addclass

--- webDesignExam/js/javascript.js
+++ webDesignExam/js/javascript.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
 	$('.button1').on('click', function(){
 		$('.img1').hide();
+		$('img').addClass('imgnew');
 	});
-
 
 
 	$('.button2').on('click', function(){
 		$('.img1').show();
+		$('img').removeClass('imgnew');
 	});
 
 });
